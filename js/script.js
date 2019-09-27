d213be4d47b0265886b3b28dc320eacf352488b2
Rhuan Carlos <pf.rhuan@gmail.com>
2019-09-27 13:01:17 -0300

    Corrigindo alguns bugs na validação dos campos

--- js/script.js
+++ js/script.js
@@ -1,42 +1,26 @@
 $(document).ready(function() {
-    $("#input-cpfcnpj").keydown(function() { 
 
-        //try catch para retirar a mascara do input
-        try {
-            $("#input-cpfcnpj").unmask();
-        } catch (e) {}
-
-        //guardo o tamanho do que foi digitado no input
-        var checkSize = $("#input-cpfcnpj").val().length;
-
-        //verificando se o valor digitado é um CPF ou um cnpj
-        if(checkSize  < 14) {
-            $("#input-cpfcnpj").mask("999.999.999-99");
-            $("#input-cpfcnpj").attr('pattern','.{14,}');
-        } else if(checkSize >= 14){
-            $("#input-cpfcnpj").mask("99.999.999/9999-99");
-            $("#input-cpfcnpj").attr('pattern','.{18,}');
+    //MASCARA CPF / CNPJ.
+    var options = {
+        onKeyPress: function (cpf, ev, el, op) {
+            var masks = ['000.000.000-000', '00.000.000/0000-00'],
+                mask = (cpf.length > 14) ? masks[1] : masks[0];
+            el.mask(mask, op);
         }
+    }
+    
+    $('#input-cpfcnpj').mask('000.000.000-000', options);
 
-        var elem = this;
-        setTimeout(function(){
-            // muda a posição do seletor
-            elem.selectionStart = elem.selectionEnd = 10000;
-        }, 0);
-        // reaplica o valor para mudar o foco
-        var currentValue = $(this).val();
-        $(this).val('');
-        $(this).val(currentValue);
-    })
 
-    $(".input-submit-form-flex").click(function() {
-        var nome = document.forms["form-flex"]["input-name"].value;
-        var tel = document.forms["form-flex"]["input-tel"].value;
-        var cep = document.forms["form-flex"]["input-cep"].value;
-        var cpfcnpj = document.forms["form-flex"]["input-cpfcnpj"].value;
-        var email = document.forms["form-flex"]["input-email"].value;
-      
-        alert("Reservation done!" + "\nName: " + nome + "\nTelefone: " + tel + "\nCEP: " + cep + 
-              "\nCPF/CNPJ: " + cpfcnpj) + "\nE-MAIL: " + email;
-    })
+    //MASCARA TELEFONE FIXO / CELULAR.
+    var SPMaskBehavior = function (val) {
+        return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+    },
+    spOptions = {
+        onKeyPress: function(val, e, field, options) {
+            field.mask(SPMaskBehavior.apply({}, arguments), options);
+        }
+    };
+    
+    $('#input-tel').mask(SPMaskBehavior, spOptions);
 });
